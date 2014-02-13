49758c6e5fecfca8a9d72b43bdaa1f83781958da
Brett Solow <besolow@stanford.edu>
2014-02-13 05:52:40 +0000

    fixed search results text for tags

--- routes/search.js
+++ routes/search.js
@@ -1,12 +1,11 @@
 var data = require('../data.json');
 
 exports.view = function(req, res) {
-<<<<<<< HEAD
 
     var queryString = req.query.queryString;
     var query = queryString.toLowerCase();
     var queryField = req.query.queryField;
-
+    var resultsText = 'Search for: ';
     var results = []
     var entries = data['entries'];
     
@@ -25,37 +24,19 @@ exports.view = function(req, res) {
         } else {
             if(entries[i]['text'].toLowerCase().indexOf(query) != -1) {
                 results.push(entries[i]);
-                console.log("found entry");
             }
         }
     }
-    console.log(results);
-=======
-    var dummySearchData = {
-        'entries' : [
-            {
-            "datetime": 1392110792690,
-            "text": "Today sucked. I was totally ready to go to bed when I got a text saying I needed to show up to this random party that I wasn't going to go to. And then I had to go because I wanted to support my friends, but it turned out to be super exhausting. I mean, parties are great, but it just isn't that great when you're ready for bed...",
-            "tags": [],         
-            "emotion": "tired"
-        },
-        {
-            "datetime": 1392110751690,
-            "text": "Had an awesome coffee date with Brett and Neli today <3 Love my team",
-            "tags": ["coffeeftw", "delirium"],          
-            "emotion": "tired"
-        },
-        {
-            "datetime": 1392108751690,
-            "text": "I had an awesome day today when I was able to quickly find the json library I was looking for. It's the small joys in life that really make it... I should learn to appreciate the little things more.",
-            "tags": ["coding"],         
-            "emotion": "happy"
-        }]
-    };
-    var queryString = req.query.queryString;
->>>>>>> 3930837f20d64664c092f589ab0483db0f64ab72
+
+    if(queryField == 'emotion' || queryField == 'tag') {
+        resultsText = 'Viewing entries tagged as: ';
+    }
+
+
     res.render('search', {
         'query': queryString,
-        'results': results
+        'results': results,
+        'resultsText': resultsText
     });
 }
+
