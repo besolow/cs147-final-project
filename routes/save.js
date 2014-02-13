d9b677222b82966482ba7f6e9c12579887e58b2e
Makiko Fujimoto <makikof@stanford.edu>
2014-02-12 22:04:13 -0800

    fixing up new entry page

--- routes/save.js
+++ routes/save.js
@@ -4,13 +4,18 @@ exports.save = function(req, res) {
 
     var rawTags = req.body.hiddenTags;
     var allTags = rawTags.split("|");
+    var emotion = req.body.emotion;
+    console.log(emotion);
     allTags.splice(0,1);
+    if (emotion==="default"){
+        emotion = "neutral";
+    }
 
     var newEntry = {
         "datetime": Date.now(),
         "text": req.body.entryText,
         "tags": allTags,
-        "emotion": "happy"
+        "emotion": emotion
     }   
     data["entries"].push(newEntry);
 
