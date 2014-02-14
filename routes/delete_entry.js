7183306b404c25b899833b2670b8b0a9159ec0ac
Makiko Fujimoto <makikof@stanford.edu>
2014-02-13 17:11:11 -0800

    Final edit for submit

--- routes/delete_entry.js
+++ routes/delete_entry.js
@@ -2,13 +2,8 @@ var data = require('../data.json');
 
 exports.deleteEntry = function(req, res) {
     var datetime = req.body.datetime;
-    console.log(datetime);
     for (entry in data['entries']){
-        console.log(data['entries'][entry]['datetime']);
-        console.log(data['entries'][entry]);
         if (data['entries'][entry]['datetime']==datetime){
-            console.log('match found');
-            console.log(data['entries'][entry]);
             data['entries'].splice(entry, 1);
             break;
         }
