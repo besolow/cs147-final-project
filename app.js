e195984dd3312b856838714f447e53f43d419133
Makiko Fujimoto <makikof@stanford.edu>
2014-02-20 22:31:49 -0800

    stuff

--- app.js
+++ app.js
@@ -95,6 +95,7 @@ app.post('/changePass', user.changePass);
 app.post('/delete_entry', delete_entry.deleteEntry);
 app.post('/save/:oldNew', save.save);
 app.get('/new_account', new_account.view);
+app.get('/support', settings.support);
 
 // Example route
 // app.get('/users', user.list);
