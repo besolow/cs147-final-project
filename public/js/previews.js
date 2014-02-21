e8cfe70837534a41f1f9496215c9b37d45b55086
Makiko Fujimoto <makikof@stanford.edu>
2014-02-20 20:20:58 -0800

    Fixed dotdotdot

--- public/js/previews.js
+++ public/js/previews.js
@@ -11,5 +11,5 @@ $(document).ready(function() {
 function initPage() {
     var queryDict = {};
     window.location.search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]})
-    console.log(queryDict);
+    $(".thumbnail").dotdotdot();
 }
