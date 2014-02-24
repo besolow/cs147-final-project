cffa2c499a07ff453bafe3f6836aa37eb9922f0f
Makiko Fujimoto <makikof@stanford.edu>
2014-02-24 02:14:27 -0800

    Changed some of the emotions to be more positive

--- routes/edit.js
+++ routes/edit.js
@@ -27,8 +27,8 @@ exports.view = function(req, res) {
                 res.send(500);
             } else {
                 var emotionArray = [];
-                var allEmotions = ["default", "happy", "sad", "excited", "angry", "tired", "meh", "annoyed", "down"];
-                var allText = ["How are you feeling?", "Happy", "Sad", "Excited", "Angry", "Tired", "Meh", "Annoyed", "Down"];
+                var allEmotions = ["default", "happy", "sad", "excited", "angry", "loved", "meh", "peaceful", "down"];
+                var allText = ["How are you feeling?", "Happy", "Sad", "Excited", "Angry", "Loved", "Meh", "Peaceful", "Down"];
                 for (var i=0; i<allEmotions.length; i++){
                     var emotion = {};
                     emotion['val'] = allEmotions[i];
