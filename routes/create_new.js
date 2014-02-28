dbe5123813b4b920d4722d96e6c5fef8715495c6
Brett Solow <besolow@stanford.edu>
2014-02-28 05:52:28 +0000

    added autocompete for entering tags

--- routes/create_new.js
+++ routes/create_new.js
@@ -1,3 +1,10 @@
 exports.view = function(req, res) {
+    var username = req.session.username;
+    if(!username){
+        var messages = req.session.messages || [];
+        messages.push(['danger', 'Please login to continue']);
+        res.redirect('/login');
+        return;
+    }
     res.render('create_new');
 }
