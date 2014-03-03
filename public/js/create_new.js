513495b3a809fb81ad87fc4640cc2aea273cb3e8
Makiko Fujimoto <makikof@stanford.edu>
2014-03-03 13:14:14 -0800

    tmp

--- public/js/create_new.js
+++ public/js/create_new.js
@@ -4,15 +4,11 @@
 $(document).ready(function() {
     $(".emotions").click(function(e){
         var emotion = $(this).closest('.emotions').attr('id');
-        console.log(emotion);
         $("#emotionField").val(emotion);
         var html = "I feel "+emotion+'<b class="caret"></b>';
-        console.log(html);
         $("#emotionDrop").html(html);
         $(".emotions").css('color', "#333");
         $(this).css('color', '#196966');
     });
 })
 
-
-emotionDrop
