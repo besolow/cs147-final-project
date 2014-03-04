7829cc9f132ddf7776b448bf4efef91c02c1537c
Makiko Fujimoto <makikof@stanford.edu>
2014-03-03 17:49:07 -0800

    minor fixes

--- public/js/create_new.js
+++ public/js/create_new.js
@@ -3,6 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
     $(".emotions").click(function(e){
+        e.preventDefault();
         var emotion = $(this).closest('.emotions').attr('id');
         $("#emotionField").val(emotion);
         var html = $(this).closest('.emotions').html();
@@ -10,10 +11,13 @@ $(document).ready(function() {
     });
 
     $(".emotionsT").click(function(e){
+        e.preventDefault();
         var emotion = $(this).closest('.emotionsT').attr('id');
         $("#emotionField").val(emotion);
         $(".emotionsT").css('color', "#333");
-        $(this).css('color', '#196966');
+        $(".emotionsT").css('font-weight', "normal");
+        $(this).css('color', '#D68189');
+        $(this).css('font-weight', 'bold');
     });
 })
 
